c8cbca8c0b73ee88f5cd935fb0fe906ff7cf5678
erennew <erenyeagermikasa84@gmail.com>
2025-05-04 02:35:46 +0530

    Create input_handlers.py

---
+++ handlers/input_handlers.py
@@ -0,0 +1,22 @@
+from pyrogram.types import CallbackQuery
+
+async def quality_set_handler(client, callback_query: CallbackQuery):
+    pass
+
+async def quality_handler(client, callback_query: CallbackQuery):
+    pass
+
+async def set_title_handler(client, callback_query: CallbackQuery):
+    pass
+
+async def set_thumb_handler(client, callback_query: CallbackQuery):
+    pass
+
+async def set_wm_handler(client, callback_query: CallbackQuery):
+    pass
+
+async def confirm_download_handler(client, callback_query: CallbackQuery):
+    pass
+
+async def set_upload_mode_handler(client, callback_query: CallbackQuery):
+    pass
